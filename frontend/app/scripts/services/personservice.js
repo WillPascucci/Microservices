eaa0c03287cd3b71abd3a2c055e078328bb3c2d6
William Pascucci <williampascucci@dyn-160-39-154-252.dyn.columbia.edu>
2017-09-22 15:40:36 -0400

    more frontend stuff

--- frontend/app/scripts/services/personservice.js
+++ frontend/app/scripts/services/personservice.js
@@ -11,9 +11,13 @@ angular.module('teapotApp')
   .service('personService', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
-    
+    this.baseURL = 'http://test-env.wgvvepxg5d.us-east-1.elasticbeanstalk.com'
 
     this.getAllPersons = function() {
-    	return $http.get('')
+    	return $http.get(this.baseURL+'/Person')
+    }
+
+    this.getPerson = function(personID) {
+    	return $http.get(this.baseURL+'/Person/'+personID)
     }
   });
