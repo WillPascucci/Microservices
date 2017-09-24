09ecd53cb78ba53d2f8be2ddc9fdc35531008f38
jiabeiyu <jiabei.yu@columbia.edu>
2017-09-24 14:49:00 -0400

    Sequelized Backend

---
+++ backend/PersonApp_Sequelize/app.js
@@ -0,0 +1,76 @@
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
+
+var log = function(entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
+
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
+    }
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
+
+var mysql = require('mysql');
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('microservices', process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
+  host: process.env.RDS_HOSTNAME,
+  port: process.env.RDS_PORT,
+  dialect: 'mysql'
+});
+
+sequelize.authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
+
+const person = sequelize.define("Person", {
+  personid: Sequelize.INTEGER,
+  firstname: Sequelize.TEXT,
+  lastname: Sequelize.TEXT,
+  age: Sequelize.INTEGER,
+  phone: Sequelize.TEXT,
+  addressUuid: Sequelize.UUID
+})
+
+sequelize.query("SELECT * FROM Person", {model: person})
+  .then(results => {
+    console.log(results[0])
+    console.log(results[0].personid)
+    console.log(results[0].firstname)
+    console.log(results[0].lastname)
+    console.log(results[0].age)
+    console.log(results[0].phone)
+    console.log(results[0].addressUuid)
+  })
