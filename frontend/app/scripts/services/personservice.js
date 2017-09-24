8f2cd838543de21baa9ad137a6cbf2d7c0ce280c
William Pascucci <williampascucci@dyn-160-39-155-113.dyn.columbia.edu>
2017-09-24 11:27:25 -0400

    minor stuff

--- frontend/app/scripts/services/personservice.js
+++ frontend/app/scripts/services/personservice.js
@@ -14,10 +14,24 @@ angular.module('teapotApp')
     this.baseURL = 'http://test-env.wgvvepxg5d.us-east-1.elasticbeanstalk.com'
 
     this.getAllPersons = function() {
-    	return $http.get(this.baseURL+'/Person')
+    	return $http.get(this.baseURL+'/person');
     }
 
     this.getPerson = function(personID) {
-    	return $http.get(this.baseURL+'/Person/'+personID)
+    	return $http.get(this.baseURL+'/person/'+personID);
+    }
+
+    this.createPerson = function(person) {
+    	return $http.post(this.baseURL+'/person');
+    }
+
+    this.changePerson = function(person) {
+    	return $http.post(this.baseURL + '/person/'+person.personID);
+    }
+
+    this.getPersonPage = function(pagenum) {
+    	return $http.get(this.baseURL +'/person', {
+    		pageNum: pagenum
+    	});
     }
   });
