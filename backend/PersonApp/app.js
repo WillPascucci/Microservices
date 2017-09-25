2ac9d9a90baff25412a7babd5d448a8e53c2cf33
Apoorv Purwar <ap3644@columbia.edu>
2017-09-25 16:19:33 -0400

    App all added

--- backend/PersonApp/app.js
+++ backend/PersonApp/app.js
@@ -5,7 +5,6 @@ var mysql = require('mysql');
 var request = require('request');
 
 
-
 var connection = mysql.createConnection({
   host     : "aa18rny5pdr87g5.cyi40ipdvtjm.us-east-1.rds.amazonaws.com",
   user     : "microservices",
@@ -15,17 +14,23 @@ var connection = mysql.createConnection({
 });
 
 app.all('*', function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+   next();
+ });
+ 
+ app.all('*', function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-	console.log('here');
-	next();
+     console.log('here');
+     next();
  });
 
 app.use(bodyParser());
 
-connection.connect(function(err) {
+connection.connect(function(err{
   if (err) {
     console.error('Database connection failed: ' + err.stack);
     connection.end();
@@ -40,49 +45,8 @@ connection.connect(function(err) {
   })
 
   app.get('/person', function (req, res) {
-	console.log('get person called')
-    connection.query("SELECT * from Person LIMIT 5;", function (err, rows) {
-      request('aagm9e2du3rm1z.cyi40ipdvtjm.us-east-1.rds.amazonaws.com:3306/address', function (error, response, body) {
-        if (response.statusCode === 200) {
-        console.log(body)
-        body = JSON.parse(body);
-        for (row in rows) {
-          for (address in body) {
-            if (body[address].uuid==rows[row].addressUuid) {
-              console.log(address);
-              rows[row]["address"] = body[address].street + ", " + body[address].city + ", " + body[address].state + " " + body[address].zipcode;
-              delete rows[row]["addressUuid"];
-            }
-            console.log("qwrq") 
-          }
-        }
-          console.log(rows)
-          res.json(rows);
-        }
-        });
-    })
-  });
-
-  app.get('/person/page/:offset', function (req, res) {
-    console.log(req.params.offset)
-    connection.query("SELECT * from Person LIMIT "+req.params.offset+", 5",  function (err, rows) {
-      request('http://localhost:8000/address', function (error, response, body) {
-        if (response.statusCode === 200) {
-        console.log(body)
-        body = JSON.parse(body);
-        for (row in rows) {
-          for (address in body) {
-            if (body[address].uuid==rows[row].addressUuid) {
-              console.log(address);
-              rows[row]["address"] = body[address].street + ", " + body[address].city + ", " + body[address].state + " " + body[address].zipcode;
-              delete rows[row]["addressUuid"];
-            }
-          }
-        }
-          console.log(rows)
-          res.json(rows);
-        }
-        });
+    connection.query("SELECT * from Person", function (err, rows) {
+      res.json(rows);
     })
   });
 
@@ -94,57 +58,35 @@ connection.connect(function(err) {
     res.send('Post call on the Person!');
     })
   });
-
-  app.get('/person/:id', function(req, res) {
-    connection.query("SELECT * from Person WHERE id=?", req.params.id, function (err, rows) {
+  //Function to fetch the address of a person using ID
+  app.get('/person/:id/address', function (req, res) {
+    connection.query("SELECT addressUuid from Person where id=?", req.params.id, function (err, rows) {
       if (rows[0]) {
-        request('aagm9e2du3rm1z.cyi40ipdvtjm.us-east-1.rds.amazonaws.com:3306/address/' + rows[0].addressUuid, function (error, response, body) {
-          console.log('error:', error);
-          console.log('statusCode:', response && response.statusCode);
-          console.log('body:', body);
-          body = JSON.parse(body);
-          rows[0]["address"] = body['street'] + ", " + body['city'] + ", " + body['state'] + " " + body['zipcode'];
-          delete rows[0]["addressUuid"];
-          res.json(rows[0]);
-        });
-      } else {
+          console.log('http://person-env.n924wyqpyp.us-east-1.elasticbeanstalk.com:8000/address/'+rows[0].addressUuid);
+          request('http://localhost:8000/address/'+rows[0].addressUuid, function (error, response, body) {
+          console.log('error:', error); // Print the error if one occurred
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          console.log('body:', body); // Print the HTML for the Google homepage.
+          res.json(JSON.parse(body));
+      })} else {
         res.send("Invalid id!");
       }
     })
-  });
+});
 
   app.put('/person/:id', function(req, res) {
-    console.log(req);
-    connection.query("UPDATE Person SET firstname=?, lastname=?, age=?, phone=?, addressUuid=? WHERE id=?", [req.body.firstname, req.body.lastname, req.body.age, req.body.phone, req.body.addressUuid, req.params.id], function (err, rows) {
     res.send('Put on Person - ' + req.params.id);
-    })
   });
 
   app.delete('/person/:id', function(req, res) {
     //Need to test this out, postman or something?
-    connection.query("DELETE FROM Person WHERE id=?", req.params.id, function (err, rows) { 
+    connection.query("DELETE FROM Person WHERE id=?", req.params.id, function (err, rows) {
       res.send('Delete on Person - ' + req.params.id);
     })
   });
 
-//Function to fetch the address of a person using ID
- app.get('/person/:id/address', function (req, res) {
-   connection.query("SELECT addressUuid from Person where id=?", req.params.id, function (err, rows) {
-     if (rows[0]) {
-         console.log('aagm9e2du3rm1z.cyi40ipdvtjm.us-east-1.rds.amazonaws.com:3306/address/'+rows[0].addressUuid);
-         request('aagm9e2du3rm1z.cyi40ipdvtjm.us-east-1.rds.amazonaws.com:3306/address/'+rows[0].addressUuid, function (error, response, body) {
-         console.log('error:', error); // Print the error if one occurred
-         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-         console.log('body:', body); // Print the HTML for the Google homepage.
-         res.json(JSON.parse(body));
-     })} else {
-       res.send("Invalid id!");
-     }
-   })
-});
-
-  app.listen(8000, function () {
-    console.log('Person app listening on port 8000!');
+  app.listen(8080, function () {
+    console.log('Person app listening on port 8080!');
   });
 
 
