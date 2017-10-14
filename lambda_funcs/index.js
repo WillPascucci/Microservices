463c40c959ad4b53cc474f39ab0254162d2dc33e
Guy Yardeni <gy2241@columbia.edu>
2017-10-14 13:16:12 -0400

    lambda attempt

---
+++ lambda_funcs/index.js
@@ -0,0 +1,76 @@
+'use strict';
+
+console.log('Loading function');
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var app = express();
+var mysql = require('mysql');
+var request = require('request');
+
+
+var connection = mysql.createConnection({
+  host     : "assignmentpart2db.cyi40ipdvtjm.us-east-1.rds.amazonaws.com",
+  user     : "microservices",
+  password : "microservices",
+  port     : 3306,
+  database : "microservices"
+});
+
+
+
+exports.handler = (event, context, callback) => {
+    console.log("HI1");
+    console.log(event);
+    console.log("HI2");
+    console.log(context);
+    console.log("HI3");
+    console.log(event.httpMethod);
+    console.log("HI4");
+    console.log(context.httpMethod);
+    console.log("HI5");
+
+
+    const done = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    switch (event.httpMethod) {
+        case 'GET':
+            console.log("IN GET");
+            connection.query("SELECT * from company", function (error, rows) {
+                console.log("IN SELECT");
+            });
+            // console.log(error);
+            // console.log(rows);
+            callback(null, {
+                statusCode: '200',
+                body: 'hiaa',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            // });
+            break;
+        case 'POST':
+            console.log("IN POST");
+            // connection.query("INSERT INTO company (name, address, type, contactName, phone, fax) VALUES (?, ?, ?, ?, ?, ?)", [event.body.name, event.body.address, event.body.type, event.body.contactName, event.body.phone, event.body.fax], function (error, rows) {
+            //     console.log(error);
+            //     console.log(rows);
+            //     callback(null, {
+            //         statusCode: error ? '400' : '200',
+            //         body: JSON.stringify("Success!"),
+            //         headers: {
+            //             'Content-Type': 'application/json'
+            //         }
+            //     });
+            // });
+            break;
+        default:
+            console.log("IN DEFAULT");
+            done(new Error(`Unsupported method "${event.httpMethod}"`));
+    }
+};
